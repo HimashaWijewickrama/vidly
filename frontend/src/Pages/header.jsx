75dc6692ee88c81f986bcc5a3f192f521515155e
myusername <himashawije5678@gmail.com>
2024-07-01 01:31:32 +0530

    header style changes

--- frontend/src/Pages/header.jsx
+++ frontend/src/Pages/header.jsx
@@ -19,7 +19,7 @@ export default function Header() {
         <Col>
           <div className="cover-poster-item">
             <img
-              src="https://w.wallhaven.cc/full/0w/wallhaven-0w2de6.jpg"
+              src="https://images.unsplash.com/photo-1655057011043-158c48f3809d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               alt="vidly-cover-poster"
             />
             <div className="cover-poster-text">
